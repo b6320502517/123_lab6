656c10a777c12729982751dced14608d5828784d
b6320502517 <sarannut.l@ku.th>
2021-03-15 17:26:30 +0700

    2

--- 2.cpp
+++ 2.cpp
@@ -1,5 +1,22 @@
 #include<stdio.h>
 int main(){
-	
+	int n=4,i,j,k=0,a=0,b=0;
+	if(n>=4&&n<=9)
+		scanf("%d",&n);
+	char x[2][n];
+	for(i=0;i<2;i++)
+		scanf(" %[^\n]s",x[i]);
+	for(i=0;i<n;i++){
+		for(j=0;j<n;j++)
+			if(x[0][i]==x[1][j]){
+				k++;
+			}
+			if(k==n)
+				a++;
+			else
+				b++;
+			
+	}
+	printf("%d %d",a,b);
 	return 0;
 }
