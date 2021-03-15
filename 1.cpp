66bc248cff30011d99a343b695b00eab15fec26c
b6320502517 <sarannut.l@ku.th>
2021-03-15 17:11:04 +0700

    1

--- 1.cpp
+++ 1.cpp
@@ -3,22 +3,27 @@ int main(){
 	int n=1,i,j,k,m;
 	if(n>=1&&n<=10000)
 		scanf("%d",&n);
-	char s[n][1000],f[n]={};
+	char s[n][1000],f[n]={},mi[n]={};
 	for(i=0;i<n;i++)
 		scanf(" %[^\n]s",s[i]);
-	for(i=0;i<n;i++){
-		for(j=0;j<26;j++){
+	for(j=0;j<26;j++){
+		for(i=0;i<n;i++){
 			if((s[i][0]==65+j)||(s[i][0]==97+j))
-				f[i]=j+1;
+				//f[i]=j+1;
+				printf("%s\n",s[i]);
 			}
 	}
-	k=f[0];
-	for(i=0;i<n;i++){
-		if(k>f[i]){
-			k=f[i];
-			m=i;
-		}			
+	/*for(i=0;i<n;i++){
+		for(j=0;j<n-i;j++){
+			if(f[j]>f[j+1]){
+			k=f[j];
+			f[j]=f[j+1];
+			mi[i]=f[j+1];
+			f[j+1]=k;
+		}
+		}	
 	}
-	printf("%s",s[m]);
+	for(i=0;i<n;i++)
+		printf("%s",s[mi[i]]);*/
 	return 0;
 }
