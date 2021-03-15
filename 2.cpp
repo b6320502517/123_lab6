fcca6d7163952be77433f4c8f05317d972e42eb4
b6320502517 <sarannut.l@ku.th>
2021-03-15 17:30:17 +0700

    2

--- 2.cpp
+++ 2.cpp
@@ -7,15 +7,11 @@ int main(){
 	for(i=0;i<2;i++)
 		scanf(" %[^\n]s",x[i]);
 	for(i=0;i<n;i++){
-		for(j=0;j<n;j++)
-			if(x[0][i]==x[1][j]){
-				k++;
-			}
-			if(k==n)
-				a++;
-			else
-				b++;
-			
+		if(x[0][i]==x[1][i])
+			a++;
+		else
+			b++;
+		
 	}
 	printf("%d %d",a,b);
 	return 0;
