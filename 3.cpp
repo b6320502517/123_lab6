27044efd0280b558ae99e19695fd360a7b52c3d1
b6320502517 <sarannut.l@ku.th>
2021-03-15 19:22:51 +0700

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -1,15 +1,26 @@
 #include<stdio.h>
 int main(){
-	long i,j,k,n,q,l=0;
+	long i,j,k,n,q,max;
 	scanf("%ld %ld",&n,&q);
 	char a[n];
 	long c[n]={};
 	long x[q],y[q];
 	for(i=0;i<q;i++){
 		scanf(" %c %ld %ld",&a[i],&x[i],&y[i]);
-		if(a[i]=='P')
-			l++;
 	}
-	for(i=0;i<q;i++)
+	for(i=0;i<q;i++){
+		if(a[i]=='U'){
+			c[x[i]]=y[i];
+		}
+		else if(a[i]=='P'){
+			max=c[x[i]];
+			for(j=x[i];j<y[i];j++){
+				if(max<c[j]){
+       				max=c[j];
+	   			}
+			}
+			printf("%d",max);
+		}
+	}
 	return 0;
 }
