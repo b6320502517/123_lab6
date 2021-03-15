3b2b3d21b7686dc639b0087b94e86016352aade3
b6320502517 <sarannut.l@ku.th>
2021-03-15 16:51:01 +0700

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,9 +1,16 @@
 #include<stdio.h>
 int main(){
 	int n=1,i,j,k;
-	char s[1000];
 	if(n>=1&&n<=10000)
 		scanf("%d",&n);
-	
+	char s[n][1000],f[n]={};
+	for(i=0;i<n;i++)
+		scanf(" %[^\n]s",s[i]);
+	for(i=0;i<n;i++){
+		for(j=0;j<26;j++){
+			if((s[i][0]==65+j)||(s[i][0]==97+j))
+				f[i]=j+1;
+			}
+	}
 	return 0;
 }
