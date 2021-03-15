43f6b963234daa4b1c5a3f3bc033875d4e8eb05d
b6320502517 <sarannut.l@ku.th>
2021-03-15 18:00:48 +0700

    4

--- 4.cpp
+++ 4.cpp
@@ -1,25 +1,35 @@
 #include<stdio.h>
 int main(){
-	int n=3,i,max,min,max1,min1;
+	int n=3,i,j,x=1,max,min;
 	if(n>2&&n<=1000)
 		scanf("%d",&n);
-	int a[n],b[n];
-	for(i=0;i<n;i++)
-		scanf("%d %d",&a[i],&b[i]);
-	max=a[0];
+	int a[n][2],b[n]={};
+	for(i=0;i<n;i++){
+		for(j=0;j<2;j++){
+			if(x>=1&&x<n)
+				scanf("%d",&x);
+			a[i][j]=x;
+		}	
+	}	
+	for(i=0;i<n;i++){
+		for(j=0;j<2;j++){
+			if((i+1)==a[i][j]){
+				b[i]++;
+			}	
+		}
+	}	
+	max=b[0];
 	min=b[0];
 	for(i=0;i<n;i++){
-       if(max<a[i]){
-       		max=a[i];
-       		max1=i;
+       if(max<b[i]){
+       		max=b[i];
 	   }
         	
         if(min>b[i]){
         	min=b[i];
-        	min1=i;
 		}
         	
    }
-   printf("%d%d",max1+1,min1+1);
+   printf("%d%d",max,min);
 	return 0;
 }
