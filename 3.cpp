dfdfb78a9d849f24b1839014e1f9985461c7b017
b6320502517 <sarannut.l@ku.th>
2021-03-15 19:17:51 +0700

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -1,12 +1,15 @@
 #include<stdio.h>
 int main(){
-	long i,j,k,n,q;
-	char a;
+	long i,j,k,n,q,l=0;
 	scanf("%ld %ld",&n,&q);
+	char a[n];
 	long c[n]={};
 	long x[q],y[q];
 	for(i=0;i<q;i++){
-		scanf()
+		scanf(" %c %ld %ld",&a[i],&x[i],&y[i]);
+		if(a[i]=='P')
+			l++;
 	}
+	for(i=0;i<q;i++)
 	return 0;
 }
